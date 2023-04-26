b7a50e73bd1778563568884991e0eb00269a48a6
JENIPHER123 <jeniperkuki@gmail.com>
2023-04-26 11:03:24 +0300

    testing landing page

--- testing/lib/authentication/landing/auth-landing.dart
+++ testing/lib/authentication/landing/auth-landing.dart
@@ -13,10 +13,55 @@ class _AuthLandingState extends State<AuthLanding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const [
-          Text('Testing Code Landing'),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+              const Image(
+              image: AssetImage('assets/logo/logo.jpg'),
+              width: 200,
+            ),  
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'MyDrive Test Mode trial',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Color.fromARGB(255, 73, 72, 71)),
+            ),
+            const Center(
+              child: Text(
+                'Testing MyDrive',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, 'auth-login');
+              },
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(300, 50),
+                  backgroundColor: const Color.fromARGB(255, 167, 21, 11),
+                  padding: const EdgeInsets.all(20),
+                  elevation: 30,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  )),
+              child: const Text(
+                'Start Testing',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
