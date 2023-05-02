44396b8822cf2db4d39172eda3c412cc31396726
JENIPHER123 <jeniperkuki@gmail.com>
2023-05-02 21:39:00 +0300

    implimenting admin analytics ui

--- mydrive/lib/admin/analytics/admin-analytics.dart
+++ mydrive/lib/admin/analytics/admin-analytics.dart
@@ -15,13 +15,112 @@ class _AdminAnalyticsState extends State<AdminAnalytics> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       drawer: const AdminSideNav(),
+      drawer: const AdminSideNav(),
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 121, 22, 15),
-        title: const Text('Analytics....'),
+        title: const Text('Analytics and Data'),
       ),
       body: Center(
-        child: Text('Admin Analytics Page'),
+        child: GridView(
+            padding: EdgeInsets.all(20),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, crossAxisSpacing: 14, mainAxisSpacing: 14),
+            children: [
+              Container(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(
+                          color: Color.fromARGB(255, 204, 202, 202), width: 3)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+                    child: Column(
+                      children: const [
+                        Image(image: AssetImage('images/chart3.gif')),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Text(
+                          'Our Profit Margins',
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(
+                          color: Color.fromARGB(255, 204, 202, 202), width: 3)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: const [
+                        Image(image: AssetImage('images/chart4.gif')),
+                        Text(
+                          'Manage Our Assets',
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(
+                          color: Color.fromARGB(255, 204, 202, 202), width: 3)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+                    child: Column(
+                      children: const [
+                        Image(image: AssetImage('images/liability1.gif')),
+                        Text(
+                          'Current Liabilities ',
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(
+                          color: Color.fromARGB(255, 204, 202, 202), width: 3)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: Column(
+                    children: const [
+                      Image(image: AssetImage('images/feedback1.gif')),
+                      Text(
+                        'customer Feedback',
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ]),
       ),
     );
   }
