4ec208d92bbec788a93b2c8ef35d06f2542072a0
JENIPHER123 <jeniperkuki@gmail.com>
2023-05-04 22:53:34 +0300

    final draft admin ui

--- mydrive/lib/admin/landing/admin-landing.dart
+++ mydrive/lib/admin/landing/admin-landing.dart
@@ -30,186 +30,193 @@ class _AdminLandingState extends State<AdminLanding> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(28.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Center(
-                      child: Text(
-                        'CONFIGURE SETTINGS',
-                        style: TextStyle(
-                            fontSize: 25,
-                            color: Color.fromARGB(255, 194, 189, 189),
-                            fontWeight: FontWeight.bold,
-                            fontFamily: AutofillHints.addressCityAndState),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(28.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Center(
+                            child: Text(
+                              'CONFIGURE SETTINGS',
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  color: Color.fromARGB(255, 194, 189, 189),
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily:
+                                      AutofillHints.addressCityAndState),
+                            ),
+                          ),
+                          const Divider(),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Card(
+                            child: ListTile(
+                              title: Text('First Quarter Of The Year'),
+                              subtitle: Row(
+                                children: [
+                                  const Text(
+                                    '2022 TOTAL PROFIT : ',
+                                  ),
+                                  const SizedBox(
+                                    width: 14,
+                                  ),
+                                  const Text('33,111,000'),
+                                  const SizedBox(
+                                    width: 32,
+                                  ),
+                                  Switch(
+                                      // This bool value toggles the switch.
+                                      value: light5,
+                                      activeColor: Colors.red,
+                                      onChanged: (bool value) {
+                                        // This is called when the user toggles the switch.
+                                        setState(() {
+                                          light5 = value;
+                                        });
+                                      })
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Card(
+                            child: ListTile(
+                              title: Text('First Quarter Of The Year'),
+                              subtitle: Row(
+                                children: [
+                                  const Text(
+                                    '2022 TOTAL PROFIT : ',
+                                  ),
+                                  const SizedBox(
+                                    width: 14,
+                                  ),
+                                  const Text('33,111,000'),
+                                  const SizedBox(
+                                    width: 32,
+                                  ),
+                                  Switch(
+                                      // This bool value toggles the switch.
+                                      value: light1,
+                                      activeColor: Colors.red,
+                                      onChanged: (bool value) {
+                                        // This is called when the user toggles the switch.
+                                        setState(() {
+                                          light1 = value;
+                                        });
+                                      })
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Card(
+                            child: ListTile(
+                              title: Text('First Quarter Of The Year'),
+                              subtitle: Row(
+                                children: [
+                                  const Text(
+                                    '2022 TOTAL PROFIT : ',
+                                  ),
+                                  const SizedBox(
+                                    width: 14,
+                                  ),
+                                  const Text('33,111,000'),
+                                  const SizedBox(
+                                    width: 32,
+                                  ),
+                                  Switch(
+                                      // This bool value toggles the switch.
+                                      value: light4,
+                                      activeColor: Colors.red,
+                                      onChanged: (bool value) {
+                                        // This is called when the user toggles the switch.
+                                        setState(() {
+                                          light4 = value;
+                                        });
+                                      })
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Card(
+                            child: ListTile(
+                              title: Text('First Quarter Of The Year'),
+                              subtitle: Row(
+                                children: [
+                                  const Text(
+                                    '2022 TOTAL PROFIT : ',
+                                  ),
+                                  const SizedBox(
+                                    width: 14,
+                                  ),
+                                  const Text('33,111,000'),
+                                  const SizedBox(
+                                    width: 32,
+                                  ),
+                                  Switch(
+                                      // This bool value toggles the switch.
+                                      value: light2,
+                                      activeColor: Colors.red,
+                                      onChanged: (bool value) {
+                                        // This is called when the user toggles the switch.
+                                        setState(() {
+                                          light2 = value;
+                                        });
+                                      })
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Card(
+                            child: ListTile(
+                              title: Text('First Quarter Of The Year'),
+                              subtitle: Row(
+                                children: [
+                                  const Text(
+                                    '2022 TOTAL PROFIT : ',
+                                  ),
+                                  const SizedBox(
+                                    width: 14,
+                                  ),
+                                  const Text('33,111,000'),
+                                  const SizedBox(
+                                    width: 32,
+                                  ),
+                                  Switch(
+                                      // This bool value toggles the switch.
+                                      value: light3,
+                                      activeColor: Colors.red,
+                                      onChanged: (bool value) {
+                                        // This is called when the user toggles the switch.
+                                        setState(() {
+                                          light3 = value;
+                                        });
+                                      })
+                                ],
+                              ),
+                            ),
+                          ),
+                          Divider(),
+                        ],
                       ),
                     ),
-                    const Divider(),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Card(
-                      child: ListTile(
-                        title: Text('First Quarter Of The Year'),
-                        subtitle: Row(
-                          children: [
-                            const Text(
-                              '2022 TOTAL PROFIT : ',
-                            ),
-                            const SizedBox(
-                              width: 14,
-                            ),
-                            const Text('33,111,000'),
-                            const SizedBox(
-                              width: 32,
-                            ),
-                            Switch(
-                                // This bool value toggles the switch.
-                                value: light5,
-                                activeColor: Colors.red,
-                                onChanged: (bool value) {
-                                  // This is called when the user toggles the switch.
-                                  setState(() {
-                                    light5 = value;
-                                  });
-                                })
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Card(
-                      child: ListTile(
-                        title: Text('First Quarter Of The Year'),
-                        subtitle: Row(
-                          children: [
-                            const Text(
-                              '2022 TOTAL PROFIT : ',
-                            ),
-                            const SizedBox(
-                              width: 14,
-                            ),
-                            const Text('33,111,000'),
-                            const SizedBox(
-                              width: 32,
-                            ),
-                            Switch(
-                                // This bool value toggles the switch.
-                                value: light1,
-                                activeColor: Colors.red,
-                                onChanged: (bool value) {
-                                  // This is called when the user toggles the switch.
-                                  setState(() {
-                                    light1 = value;
-                                  });
-                                })
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Card(
-                      child: ListTile(
-                        title: Text('First Quarter Of The Year'),
-                        subtitle: Row(
-                          children: [
-                            const Text(
-                              '2022 TOTAL PROFIT : ',
-                            ),
-                            const SizedBox(
-                              width: 14,
-                            ),
-                            const Text('33,111,000'),
-                            const SizedBox(
-                              width: 32,
-                            ),
-                            Switch(
-                                // This bool value toggles the switch.
-                                value: light4,
-                                activeColor: Colors.red,
-                                onChanged: (bool value) {
-                                  // This is called when the user toggles the switch.
-                                  setState(() {
-                                    light4 = value;
-                                  });
-                                })
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Card(
-                      child: ListTile(
-                        title: Text('First Quarter Of The Year'),
-                        subtitle: Row(
-                          children: [
-                            const Text(
-                              '2022 TOTAL PROFIT : ',
-                            ),
-                            const SizedBox(
-                              width: 14,
-                            ),
-                            const Text('33,111,000'),
-                            const SizedBox(
-                              width: 32,
-                            ),
-                            Switch(
-                                // This bool value toggles the switch.
-                                value: light2,
-                                activeColor: Colors.red,
-                                onChanged: (bool value) {
-                                  // This is called when the user toggles the switch.
-                                  setState(() {
-                                    light2 = value;
-                                  });
-                                })
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Card(
-                      child: ListTile(
-                        title: Text('First Quarter Of The Year'),
-                        subtitle: Row(
-                          children: [
-                            const Text(
-                              '2022 TOTAL PROFIT : ',
-                            ),
-                            const SizedBox(
-                              width: 14,
-                            ),
-                            const Text('33,111,000'),
-                            const SizedBox(
-                              width: 32,
-                            ),
-                            Switch(
-                                // This bool value toggles the switch.
-                                value: light3,
-                                activeColor: Colors.red,
-                                onChanged: (bool value) {
-                                  // This is called when the user toggles the switch.
-                                  setState(() {
-                                    light3 = value;
-                                  });
-                                })
-                          ],
-                        ),
-                      ),
-                    ),
-                    Divider(),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
