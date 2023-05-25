a7c919c3a4f9df4c035204eff1a335d6ddb19ca3
JENIPHER123 <jeniperkuki@gmail.com>
2023-05-25 22:15:55 +0300

    adding landing to main

--- my_drive/lib/general/landing/main-landing.dart
+++ my_drive/lib/general/landing/main-landing.dart
@@ -10,6 +10,54 @@ class MainLanding extends StatefulWidget {
 class _MainLandingState extends State<MainLanding> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Image(
+              image: AssetImage('assets/logo/logo.jpg'),
+              width: 200,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Welcome to MyDrive ',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 102, 101, 101),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'MyDrive App',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, 'routing');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 121, 22, 15),
+                minimumSize: const Size(250, 50),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+              ),
+              child: const Text(
+                'Get Started',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
