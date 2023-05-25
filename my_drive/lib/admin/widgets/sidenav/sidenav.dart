6a3e7c55edcfa749e551ce79de1935b5df0d902d
JENIPHER123 <jeniperkuki@gmail.com>
2023-05-25 23:45:04 +0300

    adding admin dashboard to final

---
+++ my_drive/lib/admin/widgets/sidenav/sidenav.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminSideNav extends StatelessWidget {
+  const AdminSideNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
