7c6874294a72e95999b1a1cc44387625ffd3dd38
JENIPHER123 <jeniperkuki@gmail.com>
2023-05-25 22:51:12 +0300

    adding general reg to final

--- my_drive/lib/general/register/main-reg.dart
+++ my_drive/lib/general/register/main-reg.dart
@@ -10,6 +10,88 @@ class MainReg extends StatefulWidget {
 class _MainRegState extends State<MainReg> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.lock,
+              color: Color.fromARGB(255, 121, 22, 15),
+              size: 80,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Register with My Drive',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 121, 120, 120),
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, 'driver-reg');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 121, 22, 15),
+                minimumSize: const Size(250, 50),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+              ),
+              child: const Text('Driver Registration'),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, 'client-reg');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 121, 22, 15),
+                minimumSize: const Size(250, 50),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+              ),
+              child: const Text('Client Registration'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Already have an account ? ",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 94, 92, 92)),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'routing');
+                  },
+                  child: const Text(
+                    'Login Here ..',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 121, 22, 15),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
